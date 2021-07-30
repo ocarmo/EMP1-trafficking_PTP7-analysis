4b85278f7e8fd66379f4c820d4366bf6b17258fc
Olivia Carmo <omscarmo@gmail.com>
2021-07-30 13:11:12 +1000

    updated from original repo

--- SEM-external-cell-texture/analysis_scripts/4_summary_calculation.py
+++ SEM-external-cell-texture/analysis_scripts/4_summary_calculation.py
@@ -41,7 +41,7 @@ features['scaled_pixels_um'] = features['image_name'].map(image_scaled_dict)
 features['pixels_nm'] = features['scaled_pixels_um']/1000
 
 # scale: area and axes lengths
-features['area_nm'] = (features['area'] / features['pixels_nm'])
+features['area_nm'] = (features['area'] / (features['pixels_nm']**2))
 features['major_axis_length_nm'] = (
     features['major_axis_length'] / features['pixels_nm'])
 features['minor_axis_length_nm'] = (
@@ -54,22 +54,33 @@ major_axis = features[features['ROI_type'] == 'knob'].groupby(
     ['image_name', 'cell_number']).mean()['major_axis_length_nm']
 
 # --------------Calculate average size of knobs per cell--------------
-av_size = features[features['ROI_type'] == 'knob'].groupby(['image_name', 'cell_number']).mean()['area_nm'].reset_index()
+av_size = features[features['ROI_type'] == 'knob'].groupby(
+    ['image_name', 'cell_number']).mean()['area_nm'].reset_index()
 
-# --------------Calculate proportion of area in 'knobs'--------------
-cell_size = features[features['ROI_type'] == 'cell'].set_index(['image_name', 'cell_number'])['area_nm']
-knob_area = features[features['ROI_type'] == 'knob'].groupby(['image_name', 'cell_number']).sum()['area_nm']
-knob_proportion = ((knob_area / cell_size)*100).reset_index().rename(columns={'area_nm': 'proportion_knob_area'})
+# --------------Calculate proportion of area in knobs--------------
+cell_size = features[features['ROI_type'] == 'cell'].set_index(
+    ['image_name', 'cell_number'])['area_nm']
+knob_area = features[features['ROI_type'] == 'knob'].groupby(
+    ['image_name', 'cell_number']).sum()['area_nm']
+knob_proportion = ((knob_area / cell_size) *
+                   100).reset_index().rename(columns={'area_nm': 'proportion_knob_area'})
 
 # --------------Calculate number of 'knobs' per cell--------------
 # NOTE: due to focus issues, cell shape changes etc, this measure is likely not valid. Generating it just in case!
-knob_count = features[features['ROI_type'] == 'knob'].groupby(['image_name', 'cell_number']).count()['area_nm'] 
-knobs_per_area = (knob_count / (cell_size/10000)).reset_index().rename(columns={'area_nm': 'knobs_per_100_nm2'})
+knob_count = features[features['ROI_type'] == 'knob'].groupby(
+    ['image_name', 'cell_number']).count()['area_nm']
+knobs_per_area = (knob_count / (cell_size/10000)
+                  ).reset_index().rename(columns={'area_nm': 'knobs_per_100_nm2'})
+
+# --------------Calculate knob density (count/area in µm) --------------
+knob_density = (knob_count / (cell_size/1000000)
+                ).reset_index().rename(columns={'area_nm': 'proportion_knob_area'})
 
 # --------------Summarise, save to csv--------------
-summary = functools.reduce(lambda left, right: pd.merge(left, right, on=['image_name', 'cell_number'], how='outer'), [av_size, cell_size.reset_index(), knob_area.reset_index(), knob_proportion, knob_count.reset_index(), knobs_per_area, minor_axis, major_axis])
+summary = functools.reduce(lambda left, right: pd.merge(left, right, on=['image_name', 'cell_number'], how='outer'), [av_size, cell_size.reset_index(
+), knob_area.reset_index(), knob_proportion, knob_count.reset_index(), knobs_per_area, knob_density, minor_axis, major_axis])
 summary.columns = ['image_name', 'cell_number', 'mean_knob_area', 'cell_size', 'total_knob_area',
-                   'knob_area_proportion', 'knob_count', 'knobs_per_100_nm2', 'mean_minor_axis', 'mean_major_axis']
+                   'knob_area_proportion', 'knob_count', 'knobs_per_100_nm2', 'knob_density_um2', 'knob_mean_minor_axis', 'knob_mean_major_axis']
 
 # make 'cell_line' column
 summary['cell_line'] = summary['image_name'].str.split('-').str[1]
